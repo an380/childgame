15261f1291c13acf068a42c9044007ef2875a10f
an380 <76869450+an380@users.noreply.github.com>
2021-01-19 19:43:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,47 @@
-var canvas;
-var music;
-var ball
-var rect1
-var rect2
-var rect3
-var rect4
-var Edges
-function preload(){
-    music = loadSound("music.mp3");
+var cat
+var mouse
+var garden
+function preload() {
+    bg = loadImage("images/garden.png");
+    tomImg1= loadAnimation("images/tomOne.png");
+    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
+    tomImg3= loadAnimation("images/tomFour.png");
+    jerryImg1=loadAnimation("images/jerryOne.png");
+    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
+    jerryImg3=loadAnimation("images/jerryFour.png");
 }
 
-
 function setup(){
-    canvas = createCanvas(800,400);
- ball=createSprite (400,20,50,50)
- ball.shapeColor = "white"
- ball.velocityX = 3   
-ball.velocityY = 3
-rect1=createSprite(750,390,200,30)
-rect1.shapeColor = "red"
-rect2=createSprite(530,390,200,30)
-rect2.shapeColor = "blue"
-rect3=createSprite(320,390,200,30)
-rect3.shapeColor = "pink"
-rect4=createSprite(100,390,200,30)
-rect4.shapeColor = "yellow"
-Edges = createEdgeSprites ()
+    createCanvas(800,800);
+    cat = createSprite(750,750,30,30)
+    mouse = createSprite(50,50,30,30)
 }
 
 function draw() {
-    background("black");
-    ball.bounceOff (Edges)
- if (childgame(ball,rect1) &&  ball.bounceOff (rect1)){
-    ball.shapeColor = "red"
-   
- }
- else if (childgame(ball,rect2) && ball.bounceOff (rect2)  
- ){
-     ball.shapeColor = "blue"
-     
- }
- else if (childgame(ball,rect3)){
-     ball.shapeColor = "pink"
-     ball.bounceOff (rect3)  
- }
- else if (childgame(ball,rect4)){
-     ball.shapeColor = "yellow"
-     ball.bounceOff (rect4)  
- }
- /*else {
-     ball.shapeColor = "white"
- }*/
-drawSprites()
-    
-}
-function childgame(object1,object2){
-    if (object2.y-object1.y<object1.height/2+object2.height/2){
-         object1.shapeColor = "white"
-         return true
-    }
-    /*else{
-        return false    
+
+    background(255);
+    /*if(keyPressed("LEFT_ARROW")){
+        cat.x = cat.x-4
+        cat.addImage(tom2)
+        mouse.addImage(jerry3)
     }*/
-    
+    if (cat.x-mouse.x<cat.width/2+mouse.width/2){
+        mouse.addImage(jerry1)
+        cat.addImage(tom1)
+    }
+
+    drawSprites();
 }
+function keyPressed(){
+
+    if(keyCode === LEFT_ARROW){
+        tom.velocityX = -5; 
+        tom.addAnimation("tomRunning", tomImg2);
+        tom.changeAnimation("tomRunning");
+        
+        jerry.addAnimation("jerryTeasing", jerryImg2);
+        jerry.frameDelay = 25;
+        jerry.changeAnimation("jerryTeasing");
+    }
+}
+
